60c82d0738f0a92daec91a1d2fabea4d9e8b3212
Kozhukhar Dmitry <dna.coj@gmail.com>
2019-03-27 18:59:57 +0300

    10

---
+++ app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -0,0 +1,35 @@
+package com.example.android.navigation
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.Navigation
+import androidx.navigation.fragment.findNavController
+import com.example.android.navigation.databinding.FragmentTitleBinding
+
+/**
+ * A simple [Fragment] subclass.
+ *
+ */
+class TitleFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        val binding: FragmentTitleBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_title, container, false)
+        binding.playButton.setOnClickListener {
+            // You can use this
+            //findNavController().navigate(R.id.action_titleFragment_to_gameFragment)
+            // or
+            Navigation.createNavigateOnClickListener(R.id.action_titleFragment_to_gameFragment)
+        }
+        return binding.root
+
+    }
+
+
+}
